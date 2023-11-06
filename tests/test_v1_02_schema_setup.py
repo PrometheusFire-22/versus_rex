09d39db0683b7333716756bb501bce64c96de0bd
Prometheus <axiologycapital@gmail.com>
2023-11-05 21:06:37 -0500

    initial schema test setup w/ pytest and psycopg2

--- tests/test_v1_02_schema_setup.py
+++ tests/test_v1_02_schema_setup.py
@@ -1,83 +1,82 @@
+import logging
+import os
 import psycopg2
 from psycopg2 import sql
-from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
-import logging
+import pytest
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-# Connection parameters - adjust these according to your setup
+# Connection parameters - obtain from environment variables
 db_params = {
-    "dbname": "your_dbname",
-    "user": "your_username",
-    "password": "your_password",
-    "host": "localhost",
-    "port": 5432
+    "dbname": os.environ.get("DB_NAME"),
+    "user": os.environ.get("DB_USER"),
+    "password": os.environ.get("DB_PASSWORD"),
+    "host": os.environ.get("DB_HOST", "localhost"),
+    "port": os.environ.get("DB_PORT", 5432)
 }
 
-def get_db_connection():
+@pytest.fixture(scope="module")
+def db_connection():
     try:
         conn = psycopg2.connect(**db_params)
-        return conn
-    except Exception as e:
-        logging.error(f"Error connecting to the database: {e}")
-        raise
+        logging.info("Database connection established.")
+        yield conn
+    except psycopg2.DatabaseError as e:
+        logging.error(f"Database connection failed: {e}")
+        pytest.fail(f"Database connection failed: {e}")
+    finally:
+        conn.close()
+        logging.info("Database connection closed.")
 
-def setup():
-    logging.info("Setting up test environment.")
-    # Setup can include creating a test database or other preparatory tasks.
-    # For this example, we are simply ensuring we can connect to the database.
-    conn = get_db_connection()
-    conn.close()
+@pytest.fixture(scope="module")
+def cursor(db_connection):
+    cur = db_connection.cursor()
+    yield cur
+    cur.close()
 
-def teardown():
-    logging.info("Tearing down test environment.")
-    # Teardown would involve cleaning up the database or any other necessary cleanup.
-    # In this simple example, there is no persistent change to clean up.
+@pytest.fixture(scope="function", autouse=True)
+def rollback_transaction(request, db_connection):
+    # Start a transaction before each test
+    db_connection.begin()
+    logging.info("Transaction started for test.")
 
-def test_schema_creation():
-    conn = get_db_connection()
-    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)  # Allow to create schema outside of a transaction
-    cursor = conn.cursor()
+    def rollback():
+        # Rollback the transaction after each test
+        db_connection.rollback()
+        logging.info("Transaction rolled back.")
+
+    request.addfinalizer(rollback)
+
+def check_role_exists(cursor, role_name):
+    try:
+        cursor.execute(sql.SQL("SELECT 1 FROM pg_roles WHERE rolname = %s;"), (role_name,))
+        return cursor.fetchone() is not None
+    except psycopg2.DatabaseError as e:
+        logging.error(f"Check role exists failed: {e}")
+        return False
+
+def test_role_exists(cursor):
+    role_name = 'oedypus'
+    assert check_role_exists(cursor, role_name), f"Role '{role_name}' does not exist."
+    logging.info(f"Role '{role_name}' confirmed to exist.")
+
+def test_schema_creation(cursor):
+    schema_name = 'email_schema'
+    role_name = 'oedypus'
 
     try:
-        # Define the schema and role
-        schema_name = 'email_schema'
-        role_name = 'oedypus'
-
-        # Check if the role exists
-        cursor.execute(sql.SQL("SELECT 1 FROM pg_roles WHERE rolname = %s;"), (role_name,))
-        role_exists = cursor.fetchone()
-        assert role_exists, f"Role '{role_name}' does not exist."
-
-        # Check if the schema already exists
         cursor.execute(sql.SQL("SELECT schema_name FROM information_schema.schemata WHERE schema_name = %s;"), (schema_name,))
-        schema_exists = cursor.fetchone()
-
-        # Create schema if it does not exist
-        if not schema_exists:
+        if cursor.fetchone() is None:
             cursor.execute(sql.SQL("CREATE SCHEMA IF NOT EXISTS {} AUTHORIZATION {};").format(
                 sql.Identifier(schema_name),
                 sql.Identifier(role_name)
             ))
+            logging.info(f"Schema '{schema_name}' created.")
 
-        # Check if the role has the correct default search path
-        cursor.execute(sql.SQL("SHOW search_path;"))
-        search_path = cursor.fetchone()
-        assert schema_name in search_path[0], f"Search path for role '{role_name}' is incorrect."
-
-        logging.info(f"Schema '{schema_name}' exists and role '{role_name}' has the correct search path.")
-    
-    except Exception as e:
-        logging.error(f"An error occurred: {e}")
-        raise
-    finally:
-        cursor.close()
-        conn.close()
-
-if __name__ == "__main__":
-    setup()
-    try:
-        test_schema_creation()
-    finally:
-        teardown()
+        cursor.execute(sql.SQL("SELECT schema_name FROM information_schema.schemata WHERE schema_name = %s;"), (schema_name,))
+        assert cursor.fetchone() is not None, f"Schema '{schema_name}' was not created."
+        logging.info(f"Schema '{schema_name}' existence confirmed.")
+    except psycopg2.DatabaseError as e:
+        logging.error(f"Schema creation test failed: {e}")
+        pytest.fail(f"Schema creation test failed: {e}")
